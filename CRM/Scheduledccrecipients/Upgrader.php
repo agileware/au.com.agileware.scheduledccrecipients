c9036ee75af168ac4c31258e87a179946ac4502d
Francis Whittle <francis@agileware.com.au>
2023-12-15 13:56:05 +1100

    CIVISCCR-4 upgrade civix boilerplate, removes deprecations

--- CRM/Scheduledccrecipients/Upgrader.php
+++ CRM/Scheduledccrecipients/Upgrader.php
@@ -4,7 +4,7 @@ use CRM_Scheduledccrecipients_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Scheduledccrecipients_Upgrader extends CRM_Scheduledccrecipients_Upgrader_Base {
+class CRM_Scheduledccrecipients_Upgrader extends CRM_Extension_Upgrader_Base {
 
   // By convention, functions that look like "function upgrade_NNNN()" are
   // upgrade tasks. They are executed in order (like Drupal's hook_update_N).
--- scheduledccrecipients.php
+++ scheduledccrecipients.php
@@ -13,15 +13,6 @@ function scheduledccrecipients_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function scheduledccrecipients_civicrm_xmlMenu(&$files) {
-  _scheduledccrecipients_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -31,96 +22,12 @@ function scheduledccrecipients_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function scheduledccrecipients_civicrm_postInstall() {
-  _scheduledccrecipients_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function scheduledccrecipients_civicrm_uninstall() {
-  _scheduledccrecipients_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function scheduledccrecipients_civicrm_enable() {
   _scheduledccrecipients_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function scheduledccrecipients_civicrm_disable() {
-  _scheduledccrecipients_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function scheduledccrecipients_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _scheduledccrecipients_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function scheduledccrecipients_civicrm_managed(&$entities) {
-  _scheduledccrecipients_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function scheduledccrecipients_civicrm_caseTypes(&$caseTypes) {
-  _scheduledccrecipients_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function scheduledccrecipients_civicrm_angularModules(&$angularModules) {
-  _scheduledccrecipients_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function scheduledccrecipients_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _scheduledccrecipients_civix_civicrm_alterSettingsFolders($metaDataFolders);
 }
 
 /**
